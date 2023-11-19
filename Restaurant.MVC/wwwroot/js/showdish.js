c8aae9ff798af13029978d28201ee4d5ab2ada63
Jakub Marczak <jj.marczak98@gmail.com>
2023-11-19 04:25:17 +0100

    pagination part 2137

--- Restaurant.MVC/wwwroot/js/showdish.js
+++ Restaurant.MVC/wwwroot/js/showdish.js
@@ -2,37 +2,29 @@ const currentScriptGetDish = document.currentScript;
 
 
 $(document).ready(function () {
-
-
-    const PageSize = 10;
+    let TotalPage = 0;
+    const PageSize = 1;
     let PageNumber = 1;
-
-    $("#dish-page-up").click(function () {
-        PageNumber = PageNumber + 1;
-    });
-
-    $("#dish-page-down").click(function () {
-        if (PageNumber === 1) {
-            PageNumber = 1
-        }
-        else {
-            PageNumber = PageNumber - 1;
-        }
-    });
-
-    
     const RenderCarWorkshopServices = (items) => {
         const TotalItems = items.Length;
+        TotalPage = items.totalItems % PageSize == 0 ? items.totalItems / PageSize : (items.totalItems /PageSize)+1
         const ItemsFrom = PageSize * (PageNumber - 1) + 1;
         const ItemsTo = (PageSize * (PageNumber - 1) + 1) + PageSize - 1;
         console.log(items);
+        console.log(TotalPage);
+
+
+        items.items.Length>0?
+            $("#dish-servivces-container").append
+            :
+            $("dish-service")
     }
 
 
 
-    const LoadDishService = () => {
+    const LoadDishService = (pageNumber) => {
         const encodeName = currentScriptGetDish.getAttribute('data-encodedName');
-        const url = `https://localhost:7152/Restaurant/${encodeName}/Dish`;
+        const url = `https://localhost:7152/Restaurant/${encodeName}/Dish?pageNumber=${pageNumber}`;
         console.log(url);
 
         $.ajax({
@@ -51,8 +43,26 @@ $(document).ready(function () {
 
 
 
-    LoadDishService();
+    LoadDishService(PageNumber);
 
 
+
+    $("#dish-page-up").click(function () {
+        if (PageNumber < TotalPage) {
+            PageNumber = PageNumber + 1;
+        }
+        
+        LoadDishService(PageNumber)
+    });
+
+    $("#dish-page-down").click(function () {
+        if (PageNumber === 1) {
+            PageNumber = 1
+        }
+        else {
+            PageNumber = PageNumber - 1;
+        }
+        LoadDishService(PageNumber)
+    });
 
 });
